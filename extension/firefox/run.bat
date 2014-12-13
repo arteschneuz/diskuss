e97ff3d42878c08d50bfb3f4cb0dea7af7e6ed39
arteschneuz <arteschneuz@users.noreply.github.com>
2014-12-13 16:35:37 +0100

    Delete run.bat

--- extension/firefox/run.bat
+++
@@ -1,7 +0,0 @@
-#https://addons.mozilla.org/en-US/firefox/addon/autoinstaller/
-
-C:\Users\xxx\Desktop\addon-sdk-1.17\addon-sdk-1.17\bin
-activate
-C:\Users\xxx\Desktop\diskuss\extension\firefox
-cfx xpi --pkgdir=C:\Users\xxx\Desktop\diskuss\extension\firefox\src
-wget --post-file=C:\Users\xxx\Desktop\diskuss\extension\firefox\diskuss.xpi http://localhost:8888/
